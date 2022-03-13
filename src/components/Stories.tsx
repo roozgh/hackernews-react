a89ae84bfa1d7e8a4072e300613ce19f364b91a1
Roozbeh <roozbeh.manchester@gmail.com>
2022-03-13 17:49:37 +0000

    replace mock data with real data

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import { Collapse } from "antd";
 import { Story } from "./Story";
-import { mockStories } from "../mock-data/stories";
+import { Item as StoryI } from "../types/hackernews";
+import { getTopStoryIDs, getTopStoriesDetails } from "../services/api";
 
 export const Stories = () => {
+  const [stories, setStories] = useState<StoryI[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const topStoryIDs = await getTopStoryIDs();
+      const topXStoryIDs = topStoryIDs.slice(0, 10);
+      const stories = await getTopStoriesDetails(topXStoryIDs);
+      const topStories = topXStoryIDs.map((storyID) =>
+        stories.find(({ id }) => id === storyID)
+      ) as StoryI[];
+      setStories(topStories);
+    };
+    fetch();
+  }, []);
+
   return (
     <Collapse accordion>
-      {mockStories.map((story, index) => (
+      {stories.map((story, index) => (
         <Collapse.Panel
           header={<Story story={story} index={index + 1} />}
           key={story.id}
